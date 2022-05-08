176a2ee9c1a9a4201e09c6c3008ec60bd16862d2
Mbaye Babacar SARR <mbayesarr.baba@gmail.com>
2022-05-08 03:37:42 +0200

    Initial commit

---
+++ src/main/java/net/mbsdev/springaxon/SpringaxonApplication.java
@@ -0,0 +1,13 @@
+package net.mbsdev.springaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaxonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaxonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mbsdev/springaxon/SpringaxonApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mbsdev.springaxon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaxonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
